8643478984a0431059fe7cba30ee7d1376d9b21a
rainhard.w <rainhard.w@gmail.com>
2011-04-20 05:13:32 +0000

    1.0: reconstruct; add locale

---
+++ src/options.js
@@ -0,0 +1,15 @@
+function setText(elementId, text) {
+	document.getElementById(elementId).innerText = text;
+}
+
+function locale() {
+	setText("header1", chrome.i18n.getMessage('option_title'));
+	setText("setting", chrome.i18n.getMessage('setting'));
+	setText("email", chrome.i18n.getMessage('email'));
+	setText("password", chrome.i18n.getMessage('password'));
+	setText("checkcycle", chrome.i18n.getMessage('checkcycle'));
+	setText("minute", chrome.i18n.getMessage('minute'));
+	setText("displayall", chrome.i18n.getMessage('displayall'));
+	setText("save-button", chrome.i18n.getMessage('save'));
+	setText("cancel-button", chrome.i18n.getMessage('cancel'));
+}
