ec2f8e568da661a602b8144896451b01049a0d36
rainhard.w <rainhard.w@gmail.com>
2013-06-30 06:48:29 +0000

    2.1: fix qq; upgrade manifest version 2

--- src/options.js
+++ src/options.js
@@ -15,3 +15,78 @@ function locale() {
 	setText("save-button", chrome.i18n.getMessage('save'));
 	setText("cancel-button", chrome.i18n.getMessage('cancel'));
 }
+
+var customDomainsTextbox;
+var emailTextBox;
+var passwordTextBox;
+var checkcycleTextBox;
+var displayallCheckBox;
+var notificationCheckBox;
+var saveButton;
+var mailserverDropList;
+
+function init() {
+  //customDomainsTextbox = document.getElementById("custom-domain");
+  emailTextBox = document.getElementById("custom-email");
+  passwordTextBox = document.getElementById("custom-password");
+  checkcycleTextBox = document.getElementById("custom-checkcycle");
+  displayallCheckBox = document.getElementById("custom-displayall");
+  notificationCheckBox = document.getElementById("custom-notify");
+  mailserverDropList = document.getElementById("custom-mailserver");
+ 
+  saveButton = document.getElementById("save-button");
+
+  //customDomainsTextbox.value = localStorage.customDomain || "";
+  emailTextBox.value = localStorage.email || "";
+  passwordTextBox.value = localStorage.password || "";
+  checkcycleTextBox.value = localStorage.checkcycle || "15";
+  displayallCheckBox.checked = localStorage.displayall == "true" || false;
+  notificationCheckBox.checked = localStorage.notification == "true" || false;
+  mailserverDropList.value = localStorage.mailserver || "163.com";
+  //if ( !localStorage.checkcycle || localStorage.checkcycle == 0 )
+	//checkcycleTextBox.value = "15";
+  //else
+	//checkcycleTextBox.value = localStorage.checkcycle.toString();
+  markClean();
+}
+
+function save() {
+  //localStorage.customDomain = customDomainsTextbox.value;
+  localStorage.email = emailTextBox.value;
+  localStorage.password = passwordTextBox.value;
+  localStorage.checkcycle = checkcycleTextBox.value;
+  localStorage.displayall = displayallCheckBox.checked;
+  localStorage.notification = notificationCheckBox.checked;
+  localStorage.mailserver = mailserverDropList.value;
+  
+  var idx_1 = emailTextBox.value.indexOf("@");
+  localStorage.mailtype = emailTextBox.value.substr(idx_1+1);
+ 
+  markClean();
+
+  chrome.extension.getBackgroundPage().init();
+}
+
+function markDirty() {
+  saveButton.disabled = false;
+}
+
+function markClean() {
+  saveButton.disabled = true;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+	document.getElementById('custom-email').addEventListener('input', markDirty);
+	document.getElementById('custom-mailserver').addEventListener('change', markDirty);
+	document.getElementById('custom-password').addEventListener('input', markDirty);
+	document.getElementById('custom-checkcycle').addEventListener('input', markDirty);
+	document.getElementById('custom-displayall').addEventListener('click', markDirty);
+	document.getElementById('custom-notify').addEventListener('click', markDirty);
+	
+	document.getElementById('save-button').addEventListener('click', save);
+	document.getElementById('cancel-button').addEventListener('click', init);
+	
+	locale();
+	init();
+});
+
